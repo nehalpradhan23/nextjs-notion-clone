6350e3a9dc7f67a95bcbbf067af3f20692d7ae78
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-03-11 15:09:10 +0530

    trashbox component, restore, permanently delete documents

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,9 +4,11 @@ import { cn } from "@/lib/utils";
 import {
   ChevronsLeft,
   MenuIcon,
+  Plus,
   PlusCircle,
   Search,
   Settings,
+  Trash,
 } from "lucide-react";
 import { usePathname } from "next/navigation";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
@@ -17,6 +19,12 @@ import { api } from "@/convex/_generated/api";
 import Item from "./item";
 import { toast } from "sonner";
 import { DocumentList } from "./document-list";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import TrashBox from "./trash-box";
 
 const Navigation = () => {
   const pathname = usePathname(); // close sidebar after click
@@ -149,6 +157,19 @@ const Navigation = () => {
         {/* documents list ======================================= */}
         <div className="mt-4">
           <DocumentList />
+          <Item onClick={handleCreate} icon={Plus} label="Add a page" />
+          {/* trash button ================================ */}
+          <Popover>
+            <PopoverTrigger className="w-full mt-4">
+              <Item label="Trash" icon={Trash} />
+            </PopoverTrigger>
+            <PopoverContent
+              className="p-0 w-72"
+              side={isMobile ? "bottom" : "right"}
+            >
+              <TrashBox />
+            </PopoverContent>
+          </Popover>
         </div>
         {/* sidebar resize border ======================================= */}
         <div
