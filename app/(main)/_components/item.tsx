728bddd217329cd5119122460ef677dad0d3c992
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-03-14 16:02:00 +0530

    blocknote implementation, onchange, add image, redirect pages, error handle

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -59,7 +59,8 @@ const Item = ({
   const onArchive = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     event.stopPropagation();
     if (!id) return;
-    const promise = archive({ id });
+    const promise = archive({ id }).then(() => router.push("/documents"));
+
     toast.promise(promise, {
       loading: "Moving to trash",
       success: "Note moved to trash",
@@ -83,7 +84,7 @@ const Item = ({
         if (!expanded) {
           onExpand?.();
         }
-        // router.push(`/documents/${documentId}`);
+        router.push(`/documents/${documentId}`);
       }
     );
     toast.promise(promise, {
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -10,7 +10,7 @@ import {
   Settings,
   Trash,
 } from "lucide-react";
-import { useParams, usePathname } from "next/navigation";
+import { useParams, usePathname, useRouter } from "next/navigation";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./user-item";
@@ -44,6 +44,7 @@ const Navigation = () => {
   const settings = useSettings();
 
   const params = useParams();
+  const router = useRouter();
 
   // close sidebar for small screen or open if made bigger
   useEffect(() => {
@@ -124,7 +125,9 @@ const Navigation = () => {
 
   // =======================================================
   const handleCreate = () => {
-    const promise = create({ title: "Untitled" });
+    const promise = create({ title: "Untitled" }).then((documentId) =>
+      router.push(`/documents/${documentId}`)
+    );
     toast.promise(promise, {
       loading: "Creating your note...",
       success: "New note created",
@@ -163,8 +166,8 @@ const Navigation = () => {
           {/* create note button =========================================== */}
           <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
-        {/* documents list ======================================= */}
         <div className="mt-4">
+          {/* documents list ======================================= */}
           <DocumentList />
           <Item onClick={handleCreate} icon={Plus} label="Add a page" />
           {/* trash button ================================ */}
