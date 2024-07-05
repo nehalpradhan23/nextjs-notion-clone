977afc846b11b707125eecece5269f4da4052049
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-07-06 01:26:53 +0530

    changes

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -11,15 +11,17 @@ import React from "react";
 export const Heading = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
   return (
-    <div className="max-w-3xl space-y-4">
-      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your Ideas, Documents, & plans. Unified. Welcome to{" "}
-        <span className="underline">Clotion</span>
-      </h1>
-      <h3 className="text-base sm:text-lg md:text-2xl font-medium">
-        Clotion is the connected workspace where <br />
-        better, faster work happens
-      </h3>
+    <div className="max-w-7xl space-y-4 lg:flex lg:gap-[100px] items-center lg:my-10">
+      <div className="max-w-4xl flex flex-col gap-10">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
+          Your Ideas, Documents, & plans. Unified. Welcome to{" "}
+          <span className="underline">Clotion</span>
+        </h1>
+        <h3 className="text-base sm:text-lg md:text-2xl font-medium">
+          Clotion is the connected workspace where <br />
+          better, faster work happens
+        </h3>
+      </div>
       {isLoading && (
         <div
           className="w-full flex items-center justify-center
